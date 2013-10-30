603a87ca62c5f97835c465ad2e516ae985666308
My Tec Bits <info@mytecbits.com>
2013-10-29 20:22:18 -0500

    Sample Upload

---
+++ MTB-Foundation-WebForms-Sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MTB_Foundation_WebForms_Sample.Global" Language="C#" %>
---
+++ MTB-Foundation-WebForms-Sample/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MTB_Foundation_WebForms_Sample.Startup))]
+namespace MTB_Foundation_WebForms_Sample
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
